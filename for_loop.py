4df33f1f0de1960f9b566bdb91a1ca9323467cc7
Hirendra K <23053666@kiit.ac.in>
2024-05-14 23:11:28 +0530

    Create for_loop.py
    testing

---
+++ for_loop.py
@@ -0,0 +1,18 @@
+#for Loop for printing a 1 to 10 number : 
+'''for i in range(1,10) :
+    print("Hello World") '''
+
+#for loop for printing natural numbers : 
+'''for i in range(1,21) :
+    print(i) '''
+
+# for loop for gapping numbers between 
+'''for i in range(1,40,2):
+    print(i) '''
+
+# for loop for even number 
+for i in range(1,20):
+     if i%2 != 0 :
+       print(i)
+
+     
